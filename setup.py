5ebe1e8513c639ef6c04907d9c4dca38e40ede18
Manuel Albarran <weap88@gmail.com>
2012-09-08 14:09:07 +0200

    Updated doc to pypi format

--- setup.py
+++ setup.py
@@ -1,25 +1,43 @@
 """
 flask-coffee2js
-----------
+===============
 
-A small Flask extension that makes it easy to use CoffeeScript with your Flask
-application.
+A small Flask extension that makes it easy to use CoffeeScript with your Flask application.
 
-Links
-`````
+Usage
+-----
 
-* `documentation <https://github.com/weapp/flask-coffee2js>`_
-* `development version
-  <https://github.com/weapp/flask-coffee2js>`_
+You can activate it by calling the ``coffee2js`` function with your Flask app as a parameter:
 
+      from flaskext.coffee2js import coffee2js
+      coffee2js(app, js_folder='js', coffee_folder='src/coffee')
+
+This will intercept the request to ``js_folder`` and compile de file if is necesary using the files from ``coffee_folder``.
+
+When you deploy your app you might not want to accept the overhead of checking the modification time of your ``.coffee`` and ``.jss`` files on each request. A simple way to avoid this is wrapping the coffee2js call in an if statement:
+
+      if app.debug:
+          from flaskext.coffee2js import coffee2js
+          coffee2js(app)
+          
+If you do this you'll be responsible for rendering the ``.coffee`` files into ``.js`` when you deploy in non-debug mode to your production server.
+
+
+- documentation_
+- development_
+
+
+.. _documentation: https://github.com/weapp/flask-coffee2js
+.. _development: https://github.com/weapp/flask-coffee2js
 
 """
+
 from setuptools import setup
 
 
 setup(
     name='flask-coffee2js',
-    version='0.1',
+    version='0.1.2',
     url='https://github.com/weapp/flask-coffee2js',
     license='MIT',
     author='Manuel Albarran',
